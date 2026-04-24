e3d19617cf9683208e6f13a1a98441415cf15c0e
Caio de Almeida Paiva <caio.almeidapaiva@hotmail.com>
2026-04-23 21:18:10 -0300

    ProjetoIntegrador2.0
    Mudanças sugeridas pelo professor

--- ProjetoIntegrador/src/JogoDaForca.java
+++ ProjetoIntegrador/src/JogoDaForca.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class JogoDaForca {
 
@@ -29,25 +29,44 @@ public class JogoDaForca {
 
 
     public static void main(String[] args) throws Exception {
-        Scanner entrada = new Scanner(System.in);
+        JogoDaForca criar = new JogoDaForca();
 
-        System.out.println("\nProjeto Integrador - Jogo da Forca");
+        JOptionPane.showMessageDialog(null, "Projeto Integrador - Jogo da Forca");
 
-        System.out.print("Digite seu nome: ");
-        String nomeJogador = entrada.next();
+        String nomeJogador = JOptionPane.showInputDialog(null, "Digite seu nome: ");
 
-        System.out.println("\nBem Vindo " + nomeJogador + " ao Jogo da Forca \nPara iniciar o jogo, digite uma das seguintes opções \n1. Como o jogo Funciona? \n2. Sair do Jogo");
-        System.out.print("Opção escolhida: ");
-        int opcao = entrada.nextInt();
+        JOptionPane.showMessageDialog(null, "\nBem Vindo " + nomeJogador + " ao Jogo da Forca");
+
+        JOptionPane.showMessageDialog(null, "\nPara iniciar o jogo, digite uma das seguintes opções \n1. Como o jogo Funciona? \n2. Sair do Jogo");
+        int opcao = Integer.parseInt(JOptionPane.showInputDialog(null, "Opção escolhida: "));
 
         switch (opcao) {
             case 1:
-                System.out.println("1. Você insere seu nome");
+                JOptionPane.showMessageDialog(null, " COMO FUNCIONA:" +
+                        "\n1 - Será solicitada a palavra que deve ser descoberta." +
+                        "\n2 - Você deverá digitar apenas uma letra para tentar advinhar a letra." +
+                        "\n3 - Após isso se a letra estiver correta aparecerá no lugar do _ que corresponde a letra descoberta." +
+                        "\n4 - Caso esteja errada, o contador de erros irá iniciar, começando em 1." +
+                        "\nATENÇÃO! Caso o limite de erros passe de 5, o usuário perde o jogo." +
+                        "\nNo final do jogo aparecerá a palavra correta, seja o usuário ganhando ou perdendo.");
 
             case 2:
+                JOptionPane.showMessageDialog(null, "Finalizando o Sistema.");
+                break;
+        }
+        JOptionPane.showMessageDialog(null, "Iniciado o Jogo.");
+
+        criar.criarPalavraEscolhida(JOptionPane.showInputDialog(null, "Digite a palavra: "));
+        int opcao2 = Integer.parseInt(JOptionPane.showInputDialog(null, "Para continuar marque 1, para sair marque 2: "));
+
+        switch (opcao2) {
+            case 1:
+                JOptionPane.showMessageDialog(null, "Iniciado o Jogo.");
+            case 2:
+                JOptionPane.showMessageDialog(null, "Finalizando o Sistema.");
+                break;
         }
 
 
-        entrada.close();
     }
 }
