11a8b52028402058e8b3fdc74643bbbe1790b98d
Caio de Almeida Paiva <caio.almeidapaiva@hotmail.com>
2026-05-03 01:55:42 -0300

    ProjetoIntegrador1.2
    Continuação do projeto

--- ProjetoIntegrador/src/JogoDaForca.java
+++ ProjetoIntegrador/src/JogoDaForca.java
@@ -1,36 +1,12 @@
 import javax.swing.JOptionPane;
+import java.util.ArrayList;
 
 public class JogoDaForca {
 
     public String palavraEscolhida;
-    public String palavraEscondida;
+    public String PalavraEscondida;
 
-    public void criarPalavraEscolhida(String palavraEscolhida) {
-        this.palavraEscolhida = palavraEscolhida;
-        for (int i = 0; i < palavraEscolhida.length(); i++) {
-            palavraEscondida += "*";
-        }
-    }
-
-    public void atualizaraPalavraEscondida(String letra) {
-        String temp = "";
-        for (int i = 0; i < this.palavraEscolhida.length(); i++) {
-
-            if (letra.equals(palavraEscolhida.substring(i, i + 1))) {
-
-                temp += letra;
-
-            } else {
-                temp += palavraEscondida.substring(0, i + 1);
-            }
-        }
-        palavraEscondida = temp; // atualiza a palavra escondida
-    }
-
-
-    public static void main(String[] args) throws Exception {
-        JogoDaForca criar = new JogoDaForca();
-
+    public void IniciarPrograma() {
         JOptionPane.showMessageDialog(null, "Projeto Integrador - Jogo da Forca");
 
         String nomeJogador = JOptionPane.showInputDialog(null, "Digite seu nome: ");
@@ -42,31 +18,110 @@ public class JogoDaForca {
 
         switch (opcao) {
             case 1:
-                JOptionPane.showMessageDialog(null, " COMO FUNCIONA:" +
+                JOptionPane.showMessageDialog(null, "COMO FUNCIONA:" +
                         "\n1 - Será solicitada a palavra que deve ser descoberta." +
                         "\n2 - Você deverá digitar apenas uma letra para tentar advinhar a letra." +
                         "\n3 - Após isso se a letra estiver correta aparecerá no lugar do _ que corresponde a letra descoberta." +
                         "\n4 - Caso esteja errada, o contador de erros irá iniciar, começando em 1." +
                         "\nATENÇÃO! Caso o limite de erros passe de 5, o usuário perde o jogo." +
                         "\nNo final do jogo aparecerá a palavra correta, seja o usuário ganhando ou perdendo.");
+                break;
 
             case 2:
                 JOptionPane.showMessageDialog(null, "Finalizando o Sistema.");
                 break;
         }
-        JOptionPane.showMessageDialog(null, "Iniciado o Jogo.");
+    }
 
-        criar.criarPalavraEscolhida(JOptionPane.showInputDialog(null, "Digite a palavra: "));
-        int opcao2 = Integer.parseInt(JOptionPane.showInputDialog(null, "Para continuar marque 1, para sair marque 2: "));
+    public void MenuDeContinuar() {
+        int opcao2 = Integer.parseInt(JOptionPane.showInputDialog(null, "Para continuar digite 1, para sair digite 2: "));
 
         switch (opcao2) {
             case 1:
-                JOptionPane.showMessageDialog(null, "Iniciado o Jogo.");
+                JOptionPane.showMessageDialog(null, "Iniciando o Jogo.");
+                break;
+
             case 2:
                 JOptionPane.showMessageDialog(null, "Finalizando o Sistema.");
                 break;
         }
+    }
 
+    public void criarPalavraEscolhida(String palavraEscolhida) {
+        this.palavraEscolhida = palavraEscolhida;
+
+        for (int i = 0; i < palavraEscolhida.length(); i++) {
+            PalavraEscondida += "_";
+
+        }
+    }
+
+    public void atualizaraPalavraEscondida(String letra) {
+        String temp = "";
+        for (int i = 0; i < this.palavraEscolhida.length(); i++) {
+
+
+            if (letra.equals(palavraEscolhida.substring(i, i + 1))) {
+                temp += letra;
+
+            } else {
+                temp += PalavraEscondida.substring(0, i + 1);
+            }
+        }
+        PalavraEscondida = temp; // atualiza a palavra escondida
+    }
+
+    public void LetraDigitada() {
+        // função para armazenar a letra digitada
+
+        ArrayList<Character> letras = new ArrayList<>();
+
+        String entrada = JOptionPane.showInputDialog(null, "Digite a letra: ");
+        char letra = entrada.charAt(0);
+
+        letras.add(letra);
+
+        JOptionPane.showMessageDialog(null, "Letras digitadas: " + letras);
+    }
+
+    public void Vidas() {
+        int vidas = 5;
+
+
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        JogoDaForca criar = new JogoDaForca();
+
+        criar.IniciarPrograma(); // Função que inicia o programa - Com menu inicial, nome e
+
+        JOptionPane.showMessageDialog(null, "Iniciado o Jogo.");
+
+        criar.criarPalavraEscolhida(JOptionPane.showInputDialog(null, "Digite a palavra: "));
+
+        criar.MenuDeContinuar(); // Menu de Escolha Ok ou Finalizar o programa - USAR PARA CADA TENTATIVA
+
+        do {
+            criar.MenuDeContinuar();
+            criar.LetraDigitada();
+
+        } while (true);
+
+
+        // Para uso futuro, não no momento
+
+        // Continuação do Programa
+
+        // criar.MenuDeContinuar(); Menu de Escolha Ok ou Finalizar o programa - USAR PARA CADA TENTATIVA
+
+        // Continuação do Programa
+
+        // criar.MenuDeContinuar(); // Menu de Escolha Ok ou Finalizar o programa - USAR PARA CADA TENTATIVA
+
+        // Continuação do Programa
+
+        // criar.MenuDeContinuar(); // Menu de Escolha Ok ou Finalizar o programa - USAR PARA CADA TENTATIVA
 
     }
 }
